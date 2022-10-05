4ea08bbeec585a55ce3145f7d9f4605c855ada40
Sergei Platov <serg1764@gmail.com>
2022-10-05 11:56:24 +0300

    поправил sql файлы

--- conf/db/2022-10-03-3-1-type_physical_inputs.sql
+++ conf/db/2022-10-03-3-1-type_physical_inputs.sql
@@ -1,5 +1,5 @@
 CREATE TABLE interface.type_physical_inputs (
                                                 id serial4 NOT NULL,
-                                                column1 varchar(20) NULL,
+                                                description varchar(20) NULL,
                                                 CONSTRAINT type_physical_inputs_pk PRIMARY KEY (id)
 );
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //require_once $_SERVER['DOCUMENT_ROOT'].'/php/vendor/autoload.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/back/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 
 /**
  * Router
