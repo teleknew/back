abcc5cf15696b065677471f6645628a3115033e5
Sergei Platov <serg1764@gmail.com>
2022-10-15 16:21:27 +0300

    добавил таблицы, добавил методы для показа-13

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,49 +1,40 @@
 <?php return array(
     'root' => array(
-        'name' => 'aa/a',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'c70b29a3a91d6caf64cf0e17da971ca1f11dd9ee',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-<<<<<<< HEAD
-=======
-        'reference' => 'c70b29a3a91d6caf64cf0e17da971ca1f11dd9ee',
+        'reference' => '6e3d488685ed5d04c35bdac50ccac8df1754b52a',
         'name' => 'aa/a',
->>>>>>> 48f11f2d0528ba1d955ff13e4b6de4e576b23344
         'dev' => true,
     ),
     'versions' => array(
         'aa/a' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'c70b29a3a91d6caf64cf0e17da971ca1f11dd9ee',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-<<<<<<< HEAD
-=======
-            'reference' => 'c70b29a3a91d6caf64cf0e17da971ca1f11dd9ee',
->>>>>>> 48f11f2d0528ba1d955ff13e4b6de4e576b23344
+            'reference' => '6e3d488685ed5d04c35bdac50ccac8df1754b52a',
             'dev_requirement' => false,
         ),
         'google/protobuf' => array(
             'pretty_version' => 'v3.21.7',
             'version' => '3.21.7.0',
-            'reference' => '56505903b395c5d38cbf4c8575bfef4f4c9d4c30',
             'type' => 'library',
             'install_path' => __DIR__ . '/../google/protobuf',
             'aliases' => array(),
+            'reference' => '56505903b395c5d38cbf4c8575bfef4f4c9d4c30',
             'dev_requirement' => false,
         ),
         'grpc/grpc' => array(
             'pretty_version' => '1.42.0',
             'version' => '1.42.0.0',
-            'reference' => '9fa44f104cb92e924d4da547323a97f3d8aca6d4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../grpc/grpc',
             'aliases' => array(),
+            'reference' => '9fa44f104cb92e924d4da547323a97f3d8aca6d4',
             'dev_requirement' => false,
         ),
     ),
